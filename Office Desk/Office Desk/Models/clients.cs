907e1535c3a4f7fa8aa2107bbf4aa13d8105ee29
vikaspatwari <vikas_patwari@hotmail.com>
2019-03-25 21:24:23 +0530

    adding database models

--- Office Desk/Office Desk/Models/clients.cs
+++ Office Desk/Office Desk/Models/clients.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Office_Desk.Models
+﻿namespace Office_Desk.Models
 {
-     public class clients
+    public class clients
     {
         public int ID { get; set; }
         public string name { get; set; }
--- Office Desk/Office Desk/Models/file_index.cs
+++ Office Desk/Office Desk/Models/file_index.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Office_Desk.Models
+﻿namespace Office_Desk.Models
 {
     public class file_index
     {
